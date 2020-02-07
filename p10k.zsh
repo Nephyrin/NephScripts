9d97b54d1d045e33b9eee375393d81c4dd003d35
John Schoenick <johns@valvesoftware.com>
2020-02-07 14:46:13 -0800

    [zsh] Move p10k hostname to left side

--- p10k.zsh
+++ p10k.zsh
@@ -36,6 +36,7 @@
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
     os_icon                 # os identifier
+    context                 # user@hostname
     dir                     # current directory
     prompt_char             # prompt symbol
   )
@@ -74,7 +75,6 @@
     azure                   # azure account name (https://docs.microsoft.com/en-us/cli/azure)
     gcloud                  # google cloud cli account and project (https://cloud.google.com/)
     google_app_cred         # google application credentials (https://cloud.google.com/docs/authentication/production)
-    context                 # user@hostname
     nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
     ranger                  # ranger shell (https://github.com/ranger/ranger)
     nnn                     # nnn shell (https://github.com/jarun/nnn)
